9f03dc362e0ec72cb4aa486de1ed3079c81c283c
AflahPS <aflahps7@gmail.com>
2023-04-14 03:33:54 +0530

    added color for pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>HireOne</title>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#50B5FF" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Find services available near you !" />
       </Head>
